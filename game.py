cc860019787fb016da89fb03c23b3355072d8f31
TigerZhu10 <143968664+TigerZhu10@users.noreply.github.com>
2024-03-31 20:12:34 -0700

    UPDATE

--- game.py
+++ game.py
@@ -2,12 +2,29 @@ import pygame
 
 pygame.init()#初始化pygame
 
-WINDOW_WIDTH = 600
-WINDOW_HELGHT = 300
+WINDOW_WIDTH = 600#x
+WINDOW_HELGHT = 300#Y
 
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HELGHT))
 
 pygame.display.set_caption("Feed the Dragon!")
+BLACK = (0,0,0)
+White = (255,255,255)
+RED = (255,0,0)
+GREEN = (0, 255,0)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
+CYAN = (0,255,255)
+MAGNETA = (255,0,255)
+
+display_surface.fill(BLUE)
+
+pygame.draw.line(display_surface, RED, (0,0), (100,100), 10)
+pygame.draw.line(display_surface, CYAN, (100,100), (300,300), 6)
+pygame.draw.circle(display_surface, MAGNETA, (WINDOW_WIDTH // 2,WINDOW_HELGHT // 2), 100, 5)
+pygame.draw.circle(display_surface, YELLOW, (WINDOW_WIDTH // 2,WINDOW_HELGHT // 2), 50, 0)
+
+
 
 game_running = True
 while game_running:
@@ -16,4 +33,6 @@ while game_running:
         if ev.type == pygame.QUIT:
             game_running = False
 
-pygame.quit()
+    pygame.display.update()
+
+pygame.quit()                                                      
