616fa44ee884761b831c5d62942c049c06e34a96
TigerZhu10 <143968664+TigerZhu10@users.noreply.github.com>
2024-04-07 11:59:53 -0700

    Added images and sounds

--- game.py
+++ game.py
@@ -8,23 +8,14 @@ WINDOW_HELGHT = 300#Y
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HELGHT))
 
 pygame.display.set_caption("Feed the Dragon!")
-BLACK = (0,0,0)
-White = (255,255,255)
-RED = (255,0,0)
-GREEN = (0, 255,0)
-BLUE = (0,0,255)
-YELLOW = (255,255,0)
-CYAN = (0,255,255)
-MAGNETA = (255,0,255)
 
-display_surface.fill(BLUE)
+dragon_left_image = pygame.image.load("dragon_left.png")
+dragon_left_rect = dragon_left_image.get_rect()
+dragon_left_rect.topleft = (0,0)
 
-pygame.draw.line(display_surface, RED, (0,0), (100,100), 10)
-pygame.draw.line(display_surface, CYAN, (100,100), (300,300), 6)
-pygame.draw.circle(display_surface, MAGNETA, (WINDOW_WIDTH // 2,WINDOW_HELGHT // 2), 100, 5)
-pygame.draw.circle(display_surface, YELLOW, (WINDOW_WIDTH // 2,WINDOW_HELGHT // 2), 50, 0)
-
-
+dragon_right_image = pygame.image.load("dragon_right.png")
+dragon_right_rect = dragon_right_image.get_rect()
+dragon_right_rect.topright = (600,0)
 
 game_running = True
 while game_running:
@@ -32,6 +23,11 @@ while game_running:
         print(ev)
         if ev.type == pygame.QUIT:
             game_running = False
+
+    display_surface.blit(dragon_right_image, dragon_right_rect)
+    display_surface.blit(dragon_left_image, dragon_left_rect)
+
+    pygame.draw.line(display_surface, (255, 255, 255), (0, 75), (WINDOW_WIDTH, 75),4)
 
     pygame.display.update()
 
