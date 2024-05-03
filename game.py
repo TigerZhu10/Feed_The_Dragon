e79b212b10c327a97018b72d92ff03b4454fffc2
TigerZhu10 <143968664+TigerZhu10@users.noreply.github.com>
2024-05-02 20:12:53 -0700

    Started the code with music and texts

--- game.py
+++ game.py
@@ -1,11 +1,12 @@
 import pygame
+import random
 
 pygame.init()#初始化pygame
 
 WINDOW_WIDTH = 600#x
-WINDOW_HELGHT = 300#Y
+WINDOW_HEIGHT = 300#Y
 
-display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HELGHT))
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 pygame.display.set_caption("Feed the Dragon!")
 
@@ -13,10 +14,37 @@ FPS = 60
 clock = pygame.time.Clock()
 
 
-dragon_left_image = pygame.image.load("dragon_left.png")
-dragon_left_rect = dragon_left_image.get_rect()
-dragon_left_rect.centerx = 300
-dragon_left_rect.centery = 150
+sound_1= pygame.mixer.Sound('music.wav')
+sound_1.play(-1)
+
+score_font = pygame.font.SysFont('calibri', 30) 
+title_font = pygame.font.SysFont('calibri', 31)
+lives_font = pygame.font.SysFont('calibri', 29)
+
+score_text = score_font.render("Score: 0", True, (255, 255, 255), (0, 0, 0))
+score_text_rect = score_text.get_rect()
+score_text_rect.topleft = (10,10) 
+
+title_text = title_font.render("The Dragon feeder", True, (30, 156, 114), (0, 0, 0))
+title_text_rect = title_text.get_rect()
+title_text_rect.center = (300,26)
+
+lives_text = lives_font.render("lives: 5", True, (255, 255, 255), (0, 0, 0))
+lives_text_rect = lives_text.get_rect()
+lives_text_rect.topright = (560,13) 
+
+
+
+
+
+dragon_right_image = pygame.image.load("dragon_right.png")
+dragon_right_rect = dragon_right_image.get_rect()
+dragon_right_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+
+coin_image = pygame.image.load("coin.png")
+coin_rect = coin_image.get_rect()
+coin_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
+
 
 game_running = True
 while game_running:
@@ -26,29 +54,29 @@ while game_running:
             game_running = False
         
     keys = pygame.key.get_pressed()
-    print(keys)
+    
 
-    if keys[pygame.K_LEFT]:
-        dragon_left_rect.x -= 5
-    if keys[pygame.K_RIGHT]:
-        dragon_left_rect.x += 5
-    if keys[pygame.K_UP]:
-        dragon_left_rect.y -= 5
-    if keys[pygame.K_DOWN]:
-        dragon_left_rect.y += 5
+    if keys[pygame.K_LEFT] and dragon_right_rect.left > 0:
+        dragon_right_rect.x -= 5
+    if keys[pygame.K_RIGHT] and dragon_right_rect.right < WINDOW_WIDTH:
+        dragon_right_rect.x += 5
+    if keys[pygame.K_UP] and dragon_right_rect.top > 0:
+        dragon_right_rect.y -= 5
+    if keys[pygame.K_DOWN] and dragon_right_rect.bottom < WINDOW_HEIGHT:
+        dragon_right_rect.y += 5
 
-    if dragon_left_rect.left < 0:
-        dragon_left_rect.left = 0
-    elif dragon_left_rect.right > WINDOW_WIDTH:
-        dragon_left_rect.right = WINDOW_WIDTH
-    if dragon_left_rect.top < 0:
-        dragon_left_rect.top = 0
-    elif dragon_left_rect.bottom > WINDOW_HELGHT:
-        dragon_left_rect.bottom = WINDOW_HELGHT
+    if dragon_right_rect.colliderect(coin_rect):
+        coin_rect.x = random.randint(0, WINDOW_WIDTH - 32)
+        coin_rect.y = random.randint(0, WINDOW_HEIGHT - 32)
     
         
     display_surface.fill((0,0,0))
-    display_surface.blit(dragon_left_image, dragon_left_rect)
+
+    display_surface.blit(dragon_right_image, dragon_right_rect)
+    display_surface.blit(score_text, score_text_rect)
+    display_surface.blit(title_text, title_text_rect)
+    display_surface.blit(lives_text, lives_text_rect)
+    display_surface.blit(coin_image, coin_rect)
     
     pygame.display.update()
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,1 @@
-if ev.type == pygame.MOUSEMOTION and pygame.MOUSEDOWN:
-        #     print(ev)
-        #     mouse_x = ev.pos[0]
-        #     mouse_y = ev.pos[1]
-        #     dragon_left_rect.centerx = mouse_x
-        #     dragon_left_rect.centery = mouse_y
+dragon_left_rect.left = 0
