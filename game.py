56be324f24c9a26d526762c2947543b0ddd8144d
TigerZhu10 <143968664+TigerZhu10@users.noreply.github.com>
2024-05-09 17:48:08 -0700

    project end

--- game.py
+++ game.py
@@ -12,17 +12,26 @@ pygame.display.set_caption("Feed the Dragon!")
 
 FPS = 60
 clock = pygame.time.Clock()
+score = 0
+lives = 5
 
 
-
-sound_1= pygame.mixer.Sound('music.wav')
+sound_1 = pygame.mixer.Sound('music.wav')
+miss_sound = pygame.mixer.Sound('sound.wav')
+get_sound = pygame.mixer.Sound('sound_1.wav')
 sound_1.play(-1)
 
 score_font = pygame.font.SysFont('calibri', 30) 
 title_font = pygame.font.SysFont('calibri', 31)
 lives_font = pygame.font.SysFont('calibri', 29)
+reset_font = pygame.font.SysFont('calibri',40)
 
-score_text = score_font.render("Score: " + str(), True, (255, 255, 255), (0, 0, 0))
+reset_text = reset_font.render("Press any key to reset the game", True, (227, 245, 66), (0, 0, 0))
+reset_text_rect = reset_text.get_rect()
+reset_text_rect.center = (300,150)
+
+
+score_text = score_font.render("Score: " + str(score), True, (255, 255, 255), (0, 0, 0))
 score_text_rect = score_text.get_rect()
 score_text_rect.topleft = (10,10) 
 
@@ -30,7 +39,7 @@ title_text = title_font.render("The Dragon feeder", True, (30, 156, 114), (0, 0,
 title_text_rect = title_text.get_rect()
 title_text_rect.center = (300,26)
 
-lives_text = lives_font.render("lives: 5", True, (255, 255, 255), (0, 0, 0))
+lives_text = lives_font.render("lives: " + str(lives), True, (255, 255, 255), (0, 0, 0))
 lives_text_rect = lives_text.get_rect()
 lives_text_rect.topright = (560,13) 
 
@@ -47,6 +56,8 @@ coin_rect = coin_image.get_rect()
 coin_rect.center = (620,200)
 coin_speed = 6
 
+
+
 game_running = True
 while game_running:
     for ev in pygame.event.get():
@@ -59,12 +70,32 @@ while game_running:
     coin_rect.x -= coin_speed
     if coin_rect.right < 0:  
         coin_rect.center = (600,200)
-        coin_rect.y = random.randint(40, 268)   
+        coin_rect.y = random.randint(40, 268) 
+        lives -= 1 
+        lives_text = lives_font.render("lives: " + str(lives), True, (255, 255, 255), (0, 0, 0))
+        miss_sound.play(0) 
+        if lives == 0:
+            sound_1.stop()
+            pause = True
+            while pause:
+                display_surface.blit(reset_text, reset_text_rect)
+                for ev in pygame.event.get():
+                    if ev.type == pygame.KEYDOWN:
+                        sound_1.play(-1)
+                        coin_speed = 6
+                        score = 0
+                        lives = 5
+                        score_text = score_font.render("Score: " + str(score), True, (255, 255, 255))
+                        lives_text = lives_font.render("Lives: " + str(lives), True, (255, 255, 255))
+                        pause = False
+                    if ev.type == pygame.QUIT:
+                        game_running = False
+                        pause = False
+                pygame.display.update()
+
+ 
+
     
-    # if keys[pygame.K_LEFT] and dragon_right_rect.left > 0:
-    #     dragon_right_rect.x -= 5
-    # if keys[pygame.K_RIGHT] and dragon_right_rect.right < WINDOW_WIDTH:
-    #     dragon_right_rect.x += 5
     if keys[pygame.K_UP] and dragon_right_rect.top > 48:
         dragon_right_rect.y -= 5
     if keys[pygame.K_DOWN] and dragon_right_rect.bottom < WINDOW_HEIGHT:
@@ -75,7 +106,12 @@ while game_running:
         coin_rect.y = random.randint(40, 268)
         coin_speed += 0.5
         if coin_speed >= 15:
-            coin_speed = 15  
+            coin_speed = 15
+        score += 1 
+        score_text = score_font.render("Score: " + str(score), True, (255, 255, 255), (0, 0, 0)) 
+        get_sound.play(0)
+             
+        
     
         
     display_surface.fill((0,0,0))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,2 @@
-if keys[pygame.K_LEFT] and dragon_right_rect.left > 0:
-    #     dragon_right_rect.x -= 5
-    # if keys[pygame.K_RIGHT] and dragon_right_rect.right < WINDOW_WIDTH:
-    #     dragon_right_rect.x += 5
+if keys[pygame.K_SPACE]:
+            #     game_running = True
